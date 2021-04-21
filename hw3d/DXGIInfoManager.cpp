5fbd38c70128d530161394fb9b592a8ffac85660
sabazov14@gmail.com <sabazov14@gmail.com>
2021-04-21 19:58:45 +0500

    added DXGI info manager class

---
+++ hw3d/DXGIInfoManager.cpp
@@ -0,0 +1,71 @@
+#include "DXGIInfoManager.h"
+#include "Window.h"
+#include "Graphics.h"
+#include <dxgidebug.h>
+#include <memory>
+
+#pragma comment(lib, "dxguid.lib")
+
+#define GFX_THROW_IF_NOINFO(hrcall) if (FAILED(hr = (hrcall))) throw Graphics::Exception{ __LINE__, WFILE, hr }
+
+DXGIInfoManager::DXGIInfoManager()
+{
+	// define function signature of DXGIGetDebugInterface
+	typedef HRESULT(WINAPI* DXGIGetDebugInterface)(REFIID, void**);
+	
+	// load the dll that contains the function DXGIGetDebugInterface
+	const auto hModDXGIDebug = LoadLibraryEx(L"dxgidebug.dll", nullptr, LOAD_LIBRARY_SEARCH_SYSTEM32);
+
+	if (hModDXGIDebug == NULL)
+	{
+		throw CHWND_LAST_EXCEPT();
+	}
+
+	// find address of DXGIGetDebugInterface in the dll
+	const auto DXGIGetDebugInterface_ = reinterpret_cast<DXGIGetDebugInterface>
+		(
+			reinterpret_cast<void*>(GetProcAddress(hModDXGIDebug, "DXGIGetDebugInterface"))
+		);
+	if (DXGIGetDebugInterface_ == NULL)
+	{
+		throw CHWND_LAST_EXCEPT();
+	}
+
+	HRESULT hr;
+	GFX_THROW_IF_NOINFO(DXGIGetDebugInterface_(__uuidof(IDXGIInfoQueue), reinterpret_cast<void**>(&pDXGIInfoQueue)));
+}
+
+DXGIInfoManager::~DXGIInfoManager()
+{
+	if (pDXGIInfoQueue)
+	{
+		pDXGIInfoQueue->Release();
+	}
+}
+
+void DXGIInfoManager::Set() noexcept
+{
+	// set the index (next) so that the next call to GetMessages()
+	// will only get errors generated after this call
+	next = pDXGIInfoQueue->GetNumStoredMessages(DXGI_DEBUG_ALL);
+}
+
+std::vector<std::string> DXGIInfoManager::GetMessages() const
+{
+	std::vector<std::string> messages;
+	const auto end = pDXGIInfoQueue->GetNumStoredMessages(DXGI_DEBUG_ALL);
+	for (auto i = next; i < end; ++i)
+	{
+		HRESULT hr;
+		SIZE_T messageLength;
+		// get the size of ith message in bytes
+		GFX_THROW_IF_NOINFO(pDXGIInfoQueue->GetMessage(DXGI_DEBUG_ALL, i, nullptr, &messageLength));
+		// allocate memory for message
+		auto bytes = std::make_unique<byte[]>(messageLength);
+		auto pMessage = reinterpret_cast<DXGI_INFO_QUEUE_MESSAGE*>(bytes.get());
+		// get the message and push its description to the vector
+		GFX_THROW_IF_NOINFO(pDXGIInfoQueue->GetMessage(DXGI_DEBUG_ALL, i, pMessage, &messageLength));
+		messages.emplace_back(pMessage->pDescription);
+	}
+	return messages;
+}
---
+++ hw3d/DXGIInfoManager.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "ChiliWin.h"
+#include <vector>
+#include <string>
+
+class DXGIInfoManager
+{
+public:
+	DXGIInfoManager();
+	~DXGIInfoManager();
+	DXGIInfoManager(const DXGIInfoManager&) = delete;
+	DXGIInfoManager& operator=(const DXGIInfoManager&) = delete;
+	void Set() noexcept;
+	std::vector<std::string> GetMessages() const;
+private:
+	unsigned long long next = 0u;
+	struct IDXGIInfoQueue* pDXGIInfoQueue = nullptr;
+};
